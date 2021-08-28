7a39bdd0e99d64b6dcf86f001f0b327746e9aba8
Anik Singha <86737704+AnikSingha@users.noreply.github.com>
2021-08-28 12:03:17 -0400

    Create Home.py

---
+++ apps/Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def app():
+    st.title('About Me')
+
+    st.write('My name is Anik. I\'m a a freshman at Hunter College studying Computer Science')
+
+    st.write('On this website are all my python and data science projects')
+
+    st.write("Heres my github https://github.com/AnikSingha?tab=repositories")
