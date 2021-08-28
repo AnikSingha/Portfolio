620d35feb655f66aff3966d8615ebedc3a33cf3b
Anik Singha <86737704+AnikSingha@users.noreply.github.com>
2021-08-28 11:41:16 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from Multi_App import MultiApp
+from apps import Home, titanic# import your app modules here
+
+st.set_page_config(page_title="My Projects")
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+
+app = MultiApp()
+
+st.markdown("""
+# My Projects
+""")
+
+# Add all your application here
+app.add_app("About Me", Home.app)
+app.add_app("Titanic Log Regression", titanic.app)
+# The main app
+app.run()
