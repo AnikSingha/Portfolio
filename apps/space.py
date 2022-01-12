69fc6b475ddb3001caa04aa5ede1f687a9dae249
Anik Singha <86737704+AnikSingha@users.noreply.github.com>
2022-01-12 11:19:59 -0500

    Add files via upload
    Add Space Invaders to the portfolio

---
+++ apps/space.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+def app():
+    st.title('Space Invaders')
+
+    st.write('I recreated the classic arcade game Space Indavers using the PhaserJS javascript framework')
+
+    st.write('Visit this link to play the game')
+
+    st.write("https://spaceinvaderstime.herokuapp.com/")
+    
+    st.write("Use the left and right arrow keys to move and the spacebar to shoot")
+
