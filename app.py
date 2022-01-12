37c13f7e95dd57e84db4bfa530d709b6f4c3f17b
Anik Singha <86737704+AnikSingha@users.noreply.github.com>
2022-01-12 11:21:09 -0500

    Update app.py
    Include space

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from Multi_App import MultiApp
-from apps import Home, titanic# import your app modules here
+from apps import Home, titanic, space# import your app modules here
 
 st.set_page_config(page_title="My Projects")
 
@@ -22,5 +22,6 @@ st.markdown("""
 # Add all your application here
 app.add_app("About Me", Home.app)
 app.add_app("Titanic Log Regression", titanic.app)
+app.add_app("Space Invaders", space.app)
 # The main app
 app.run()
